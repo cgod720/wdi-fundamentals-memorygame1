546fb90acc3650fc3182472f58756c3a8c3f2d2a
Carlos Godoy <CarlosGodoy@Carloss-MacBook-Air.local>
2018-11-26 01:43:09 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,7 +25,7 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[2]) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 		} else {
 			alert("Sorry, try again");
@@ -34,23 +34,29 @@ var checkForMatch = function() {
 }
 
 
-var flipCard = function (cardId) {
-	
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id')
 	
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
+	this.setAttribute('src', cards[cardId].cardImage);	
+
 	checkForMatch();
 	
 };
 
-flipCard(0);
-flipCard(2);
-
-
-
-//alert('Hello, friends.');
+var createBoard = function () {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
 
 
